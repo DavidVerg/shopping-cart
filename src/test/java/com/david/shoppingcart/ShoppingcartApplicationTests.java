ab9bca9d4ebaccf94a06f97a7fbc1173572d8dce
DavidVerg <david.vergara82141@gmail.com>
2021-12-17 11:58:41 -0500

    First commit

---
+++ src/test/java/com/david/shoppingcart/ShoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
